b01f7cd14cc66f8cfd8481f48703ca935a10abd2
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2021-06-21 19:37:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,14 @@
 provider "google" {
-credentials = file ("./creds/creds.json")
-project = "nasalab"
-region = "us-central1"
-
+  credentials = file ("./creds/creds.json")
+  project = "nasalab"
+  region = "us-central1"
 }
+resource "google_compute_instance" "vm-instance" {
+  name = "test=vm"
+  zone = "us-central1-a"
+  machinetype = "f1-micro"
 
-resource "google_compute_Instance" "vm-instance" {
-
-name = "test=vm"
-zone = "us-central1-a"
-Machine = "f1-micro"
-
-network-interface {
+network_interface {
 
 name = "default"
 
