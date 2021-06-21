fc282b7730cddd5a8719bb5ce69f35b836312aba
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2021-06-21 19:41:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,11 @@ resource "google_compute_instance" "vm-instance" {
   name = "test=vm"
   zone = "us-central1-a"
   machinetype = "f1-micro"
-
+ boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
 network_interface {
 
 name = "default"
