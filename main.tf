202a4dea1d35acf93241552f71c2448517232713
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2021-06-21 21:09:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-a"
 }
-data "google_compute_instance" "appserver" {
+data "google_compute_instance" "vm-instance" {
   name = "primary-application-server"
   zone = "us-central1-a"
 }
