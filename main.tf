b4cb954b15b24fe216a522d7fdd665b441711961
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2021-06-21 16:09:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+
+project = "nasalab"
+region = "us-central1"
+
+}
+
+resource "google_compute_Instance" "vm-instance" {
+
+name = "test=vm"
+zone = "us-central1-a"
+Machine = "f1-micro"
+
+network-interface {
+
+name = "default"
+
+}
+
+)
