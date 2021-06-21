a36eff231fa0838f05a9a9c0427db551a3166709
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2021-06-21 21:11:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 provider "google" {
   project = "nasalab-316914"
+  credentials = file('./creds/creds.json')
   region  = "us-central1"
   zone    = "us-central1-a"
 }
 data "google_compute_instance" "vm-instance" {
-  name = "primary-application-server"
+  name = "test-vm"
   zone = "us-central1-a"
 }
