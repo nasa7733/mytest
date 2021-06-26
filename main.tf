2347bcc2c3b4258056ca5aa41bfd5d7b752b92cb
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2021-06-26 23:33:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,28 @@ provider "google" {
 }
 
 resource "google_compute_instance" "vm-instance" {
-  name         = "test"
+  name         = "test-vm-central1-b"
   machine_type = "f1-micro"
   zone    = "us-central1-b"
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+    
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+  resource "google_compute_instance" "vm-instance" {
+  name         = "test-vm-central-a"
+  machine_type = "f1-micro"
+  zone    = "us-central1-a"
 
 
   boot_disk {
