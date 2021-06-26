f6766b65aa1f1aa4220261d6ed62e1a69d92aef4
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2021-06-26 23:43:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ provider "google" {
 resource "google_compute_instance" "vm-instance" {
   count = 2
   name    = "test-vm-central1-b"
+  name   = "test-vm"
   machine_type = "f1-micro"
   zone    = "us-central1-b"
 
